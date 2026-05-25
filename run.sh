c1a521c8241deda3116a0be97b0a8ddbb8403609
CrochetCrafting <poukyle@cassiaschools.org>
2026-05-25 23:00:18 +0000

    uh.l

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms256M -Xmx450M -XX:+UseG1GC -XX:+UnlockDiagnosticVMOptions -XX:+G1DiscardRSetsBeforeCompaction -jar paper-1.12.2.jar nogui
+java -Xms512M -Xmx1G -jar paper-1.12.2.jar nogui
